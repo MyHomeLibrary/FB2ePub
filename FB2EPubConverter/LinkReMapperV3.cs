e385728526362008f2ae6cf7720928ad4e8871f6
Lord KiRon <lordkiron@gmail.com>
2015-04-15 15:12:09 +0300

    Additional fixes to links from tables and text

--- FB2EPubConverter/LinkReMapperV3.cs
+++ FB2EPubConverter/LinkReMapperV3.cs
@@ -17,22 +17,22 @@ namespace FB2EPubConverter
         private readonly string _idString;
         private readonly HTMLItem _linkTargetItem;
         private readonly BaseXHTMLFileV3 _linkTargetDocument;
-        private readonly IHTMLItem _anchorParent;
+        private readonly IHTMLItem _linkParentContainer;
         private readonly KeyValuePair<string, List<Anchor>> _link;
         private readonly BookStructureManager _structureManager;
 
         private int _linksCount;
 
-        public LinkReMapperV3(KeyValuePair<string, List<Anchor>> link, Dictionary<string, HTMLItem> ids, BookStructureManager structureManager)
+        public LinkReMapperV3(KeyValuePair<string, List<Anchor>> link, IDictionary<string, HTMLItem> ids, BookStructureManager structureManager)
         {
             _link = link;
             _structureManager = structureManager;
             _idString = ReferencesUtils.GetIdFromLink(link.Key); // Get ID of a link target;
             _linkTargetItem = ids[_idString]; // get object targeted by link
             _linkTargetDocument = GetIDParentDocument(structureManager, _linkTargetItem); // get parent document (file) containing targeted object
-            if (_linkTargetDocument != null)
+            if (_linkTargetDocument != null) // if link target container document (document containing item with ID we jump to) found 
             {
-                _anchorParent = DetectParentContainer(_linkTargetItem); // get parent container of link target item
+                _linkParentContainer = DetectItemParentContainer(_linkTargetItem); // get parent container of link target item
             }
         }
 
@@ -43,7 +43,7 @@ namespace FB2EPubConverter
                 Logger.Log.Error(string.Format("Internal consistency error - Used ID ({0}) has to be in one of the book documents objects", _linkTargetItem));
                 return;
             }
-            if (_anchorParent == null) // if no parent container found , means the link is directly to document , which can't be , so we ignore
+            if (_linkParentContainer == null) // if no parent container found , means the link is directly to document , which can't be , so we ignore
             {
                 Logger.Log.Error(string.Format("Internal consistency error - target link item ( {0} )has no parent container", _linkTargetItem));
                 return;
@@ -125,11 +125,11 @@ namespace FB2EPubConverter
                     anchor.HRef.Value = GenerateFarLinkReference(_idString, _linkTargetDocument.FileName); // update reference link for an anchor, "far" one (with, pointing to another file name)
                     backlinkRef = GenerateFarLinkReference(anchor.GlobalAttributes.ID.Value as string, anchorDocument.FileName); // in case we going to insert backlin - create a "far" reference
                 }
-                var backLinkAnchor = new Anchor(_anchorParent.HTMLStandard);
+                var backLinkAnchor = new Anchor(_linkParentContainer.HTMLStandard);
                 backLinkAnchor.HRef.Value = backlinkRef;
                 backLinkAnchor.GlobalAttributes.Class.Value = ElementStylesV3.NoteAnchor;
-                _anchorParent.Add(new EmptyLine(_anchorParent.HTMLStandard));
-                _anchorParent.Add(backLinkAnchor);
+                _linkParentContainer.Add(new EmptyLine(_linkParentContainer.HTMLStandard));
+                _linkParentContainer.Add(backLinkAnchor);
                 _linksCount++;
                 backLinkAnchor.Add(new SimpleHTML5Text(backLinkAnchor.HTMLStandard) { Text = (_link.Value.Count > 1) ? string.Format("(<< back {0})  ", _linksCount) : string.Format("(<< back)  ") });
             }
@@ -157,16 +157,25 @@ namespace FB2EPubConverter
         /// </summary>
         /// <param name="referencedItem"></param>
         /// <returns></returns>
-        private IHTMLItem DetectParentContainer(IHTMLItem referencedItem)
+        private IHTMLItem DetectItemParentContainer(IHTMLItem referencedItem)
         {
-            if (referencedItem is IBlockElement)
+            if (referencedItem is IBlockElement) // if item itself is container - return it
             {
                 return referencedItem;
             }
-            if (referencedItem.Parent is IBlockElement)
+            if (referencedItem.Parent != null)
             {
-                return referencedItem.Parent;
+                if (referencedItem.Parent is IBlockElement) // if item is located inside container
+                {
+                    return referencedItem.Parent;
+                }
+                if (referencedItem.Parent is TextBasedElement) // if parent is text, i's ok for container
+                {
+                    return referencedItem.Parent;
+                }
+                return DetectItemParentContainer(referencedItem.Parent); // go up the inclusion chain
             }
+
             return null;
         }
 
