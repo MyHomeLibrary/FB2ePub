b55a3bf23f82827ad9999b4ae9b03181b98010c6
Lord KiRon <lordkiron@gmail.com>
2015-07-20 12:54:49 +0300

    Added NUnit support
    Changed tester code from custom testers to NUnit 3 (beta)

---
+++ Testers/ConverterTester/Fb2EPubTester.cs
@@ -0,0 +1,34 @@
+﻿using ConverterContracts.Settings;
+using ConverterTester.Tests;
+using NUnit.Framework;
+
+namespace ConverterTester
+{
+    [TestFixture]
+    class Fb2EPubTester
+    {
+        //private const string TestFileName = "Test_001.fb2";
+
+        [Test]
+        public void TestConfig()
+        {
+            var test = new ConfigFileTester(); // configuration file tester
+            test.Test();
+        }
+
+
+        [Test]
+        public void TestV2()
+        {
+            var test = new EPubConversionTester(EPubVersion.V2); // V2 conversion test 
+            test.Test();
+        }
+
+        [Test]
+        public void TestV3()
+        {
+            var test = new EPubConversionTester(EPubVersion.V3); // V3 conversion test 
+            test.Test();
+        }
+   }
+}
--- Testers/ConverterTester/Tests/ConfigFileTester.cs
+++ Testers/ConverterTester/Tests/ConfigFileTester.cs
@@ -1,40 +1,20 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Fb2epubSettings;
 
 namespace ConverterTester.Tests
 {
-    class ConfigFileTester : ITester
+    class ConfigFileTester
     {
-        private Exception _lastException;
-
-        public bool Test()
+        public void Test()
         {
-            try
-            {
-                var converterFile = new ConverterSettingsFile();
-                string fileName = Path.GetTempFileName();
-                converterFile.Settings.SetupDefaults();
-                converterFile.Save(fileName);
-                converterFile.Load(fileName);
-                File.Delete(fileName);
-            }
-            catch(Exception ex)
-            {
-                _lastException = ex;
-                return false;
-            }
-            return true;
+            var converterFile = new ConverterSettingsFile();
+            string fileName = Path.GetTempFileName();
+            converterFile.Settings.SetupDefaults();
+            converterFile.Save(fileName);
+            converterFile.Load(fileName);
+            File.Delete(fileName);
         }
 
-        public string Name
-        {
-            get { return "Configuration File tester"; }
-        }
 
-        public Exception TestError
-        {
-            get { return _lastException; }
-        }
     }
 }
